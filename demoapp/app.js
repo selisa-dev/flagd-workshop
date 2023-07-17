8b5e7da403e61d8da48e2ab1a4fec703633489d9
Sascha Seliger <sascha.seliger@essquare.de>
2023-07-17 15:01:11 +0200

    add opentelementry

--- demoapp/app.js
+++ demoapp/app.js
@@ -15,6 +15,34 @@ app.get ("/healthz", async(req, res) => {
 })
 
 /**
+ * OpenTelemetry Code
+ */
+const { OpenTelemetryHook } = require('@openfeature/open-telemetry-hook');
+
+/*instrumentation.js*/
+const opentelemetry = require("@opentelemetry/sdk-node");
+const {getNodeAutoInstrumentations,} = require("@opentelemetry/auto-instrumentations-node");
+const {OTLPTraceExporter} = require("@opentelemetry/exporter-trace-otlp-grpc");
+const {OTLPMetricExporter} = require("@opentelemetry/exporter-metrics-otlp-grpc");
+const {PeriodicExportingMetricReader} = require('@opentelemetry/sdk-metrics');
+const {Resource} = require('@opentelemetry/resources');
+const {SemanticResourceAttributes} = require('@opentelemetry/semantic-conventions');
+
+const otelServiceName = process.env.OTEL_SERVICE_NAME || 'defaultService'
+
+const sdk = new opentelemetry.NodeSDK({
+    traceExporter: new OTLPTraceExporter({}),
+    metricReader: new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter({}),
+    }),
+    instrumentations: [getNodeAutoInstrumentations()],
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: otelServiceName
+    })
+});
+sdk.start();
+
+/**
  * Minimal application code
  */
 
